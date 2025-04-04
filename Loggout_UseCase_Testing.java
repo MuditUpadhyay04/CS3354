30779037b4912770ce87efe309f805fcb6c24a2b
purav100000 <pxp220084@utdallas.edu>
2025-04-04 11:57:39 -0500

    Update Loggout_UseCase_Testing.java

--- Loggout_UseCase_Testing.java
+++ Loggout_UseCase_Testing.java
@@ -7,19 +7,19 @@ class Loggout_UseCase_Testing {
 
 
     @Test
-    void loggout_TC1() { // User is logged in (valid session)
+    void loggout_TC1() { // User is logged in (valid)
         assertEquals("Logout successful", Logout.loggout("123123123"));
     }
 
 
     @Test
-    void loggout_TC2() { // User is not logged in
+    void loggout_TC2() { // User is not logged in(Invalid)
         assertEquals("No active session found", Logout.loggout("999999999"));
     }
 
 
     @Test
-    void loggout_TC3() { // Exceptional case - session input is null
+    void loggout_TC3() { // Session input is null(Exceptional)
         assertEquals("Invalid session", Logout.loggout(null));
     }
 
